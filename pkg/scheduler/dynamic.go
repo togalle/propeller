ce79fdf196a14af0aee654e671c4d381d065b76a
Tomas Galle <tomas.galle@ugent.be>
2026-03-16 22:32:31 +0100

    :sparkles: First generation function

--- pkg/scheduler/dynamic.go
+++ pkg/scheduler/dynamic.go
@@ -1,9 +1,14 @@
 package scheduler
 
 import (
+	"math/rand"
+
 	"github.com/absmach/propeller/pkg/proplet"
 	"github.com/absmach/propeller/task"
 )
+
+const WEIGHT_MIN = -10
+const WEIGHT_MAX = 10
 
 type dynamicScheduler struct {
 }
@@ -14,4 +19,29 @@ func NewDynamic() Scheduler {
 
 func (c *dynamicScheduler) SelectProplet(t task.Task, proplets []proplet.Proplet) (proplet.Proplet, error) {
 	return proplet.Proplet{}, nil
+}
+
+// TODO: call this as a goroutine
+func train() ([4]float64, error) {
+	// Initialize weights
+	var generation [50][4]float64 = CreateFirstGeneration()
+
+	// Loop over generations
+	for n := range 50 {
+
+	}
+
+	// Return final best chromosome
+
+	return generation[0], nil
+}
+
+func CreateFirstGeneration() [50][4]float64 {
+	var generation [50][4]float64
+	for chromosome := range 50 {
+		for i := range 4 {
+			generation[i][chromosome] = WEIGHT_MIN + rand.Float64()*(WEIGHT_MAX-WEIGHT_MIN)
+		}
+	}
+	return generation
 }
