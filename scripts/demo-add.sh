be67b0bfde55d7d23b44e259feca3c5d5b436325
Tomas Galle <tomas.galle@ugent.be>
2026-06-25 10:30:02 +0200

    :fire: Cleanup

--- scripts/demo-add.sh
+++ scripts/demo-add.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
 
-TASK_RESPONSE=$(curl -s -X POST "http://localhost:7070/tasks" \
-    -H "Content-Type: application/json" \
-    -d '{"name": "matrix_mul", "inputs": [200], "scheduler": "dynamic", "weights": {"cpu_percent": 1.0, "distance": 0.1}}')
-
-TASK_ID=$(echo "$TASK_RESPONSE" | jq -r '.id')
-
-curl -s -X PUT "http://localhost:7070/tasks/$TASK_ID/upload" \
-    -F 'file=@/home/tomasgalle/UGent/thesis/propeller/build/matrix-mul.wasm' | jq
-
-curl -s -X GET "http://localhost:7070/tasks/$TASK_ID" | jq 'del(.file)'
-
-curl -s -X POST "http://localhost:7070/tasks/$TASK_ID/start" | jq
-
-sleep 5
-
-curl -s -X GET "http://localhost:7070/tasks/$TASK_ID" | jq 'del(.file)'
-
 # TASK_RESPONSE=$(curl -s -X POST "http://localhost:7070/tasks" \
 #     -H "Content-Type: application/json" \
-#     -d '{"name": "naive_fib", "inputs": [44], "scheduler": "dynamic", "weights": {"cpu_percent": 1.0, "distance": 0.1}}')
+#     -d '{"name": "matrix_mul", "inputs": [200], "scheduler": "dynamic", "weights": {"cpu_percent": 1.0, "distance": 0.1}}')
 #
 # TASK_ID=$(echo "$TASK_RESPONSE" | jq -r '.id')
 #
 # curl -s -X PUT "http://localhost:7070/tasks/$TASK_ID/upload" \
-#     -F 'file=@/home/tomasgalle/UGent/thesis/propeller/build/naive-fib.wasm' | jq
+#     -F 'file=@/home/tomasgalle/UGent/thesis/propeller/build/matrix-mul.wasm' | jq
 #
 # curl -s -X GET "http://localhost:7070/tasks/$TASK_ID" | jq 'del(.file)'
 #
@@ -33,3 +16,20 @@ curl -s -X GET "http://localhost:7070/tasks/$TASK_ID" | jq 'del(.file)'
 # sleep 5
 #
 # curl -s -X GET "http://localhost:7070/tasks/$TASK_ID" | jq 'del(.file)'
+
+TASK_RESPONSE=$(curl -s -X POST "http://localhost:7070/tasks" \
+    -H "Content-Type: application/json" \
+    -d '{"name": "naive_fib", "inputs": [40], "scheduler": "roundrobin"}')
+
+TASK_ID=$(echo "$TASK_RESPONSE" | jq -r '.id')
+
+curl -s -X PUT "http://localhost:7070/tasks/$TASK_ID/upload" \
+    -F 'file=@/home/tomasgalle/UGent/thesis/propeller/build/naive-fib.wasm' | jq
+
+curl -s -X GET "http://localhost:7070/tasks/$TASK_ID" | jq 'del(.file)'
+
+curl -s -X POST "http://localhost:7070/tasks/$TASK_ID/start" | jq
+
+sleep 3
+
+curl -s -X GET "http://localhost:7070/tasks/$TASK_ID" | jq 'del(.file)'
